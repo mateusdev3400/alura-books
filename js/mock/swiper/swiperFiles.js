21ac2fef5b5e66ae3e59fce208b0d81b4b07d8bc
Mateus Maciel <mateusdev3400@gmail.com>
2023-12-23 13:03:51 -0300

    feat: pagination swiper initialization and styling

---
+++ js/mock/swiper/swiperFiles.js
@@ -0,0 +1,10 @@
+export const swiperFiles = [
+    { id: 1, src: "apache-kafka", title: "Apache Kafka" },
+    { id: 2, src: "portugol", title: "Portugol" },
+    { id: 3, src: "tuning", title: "Tuning" },
+    { id: 4, src: "javascript", title: "JavaScript" },
+    { id: 5, src: "gestao", title: "Gestão" },
+    { id: 6, src: "lideranca", title: "Liderança" },
+    { id: 7, src: "gestao2", title: "Gestão 2" },
+    { id: 8, src: "metricas-ageis", title: "Métricas ágeis" },
+];
